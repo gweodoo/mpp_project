1194c0aa2d0a7823c3e02817b23bec18320d77f8
pierreghazal <pg.ghazal@gmail.com>
2014-08-01 12:29:27 +0200

    First commit !

---
+++ Mpp_Project/src/dk/aau/mpp_project/activity/MainActivity.java
@@ -0,0 +1,34 @@
+package dk.aau.mpp_project.activity;
+
+import dk.aau.mpp_project.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		int id = item.getItemId();
+
+		switch (id) {
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
