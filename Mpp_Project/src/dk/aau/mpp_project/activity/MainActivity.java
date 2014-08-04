aea8afed743ecf1b720b1550b0d1a85cec71a0b0
pierreghazal <pg.ghazal@gmail.com>
2014-08-04 20:31:12 +0200

    Login Activity + Logout Button
    The login and logout should work correctly.

--- Mpp_Project/src/dk/aau/mpp_project/activity/MainActivity.java
+++ Mpp_Project/src/dk/aau/mpp_project/activity/MainActivity.java
@@ -1,18 +1,51 @@
 package dk.aau.mpp_project.activity;
 
-import dk.aau.mpp_project.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.facebook.Session;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
+
+import dk.aau.mpp_project.R;
+
+/**
+ * URL Example
+ * https://github.com/ParsePlatform/IntegratingFacebookTutorial/blob/master/
+ * IntegratingFacebookTutorial
+ * -Android/src/com/parse/integratingfacebooktutorial/UserDetailsActivity.java
+ * 
+ */
 
 public class MainActivity extends Activity {
+
+	private Button	logoutButton;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		logoutButton = (Button) findViewById(R.id.logoutButton);
+		logoutButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onLogoutButtonClicked();
+			}
+		});
+
+		// Fetch Facebook user info if the session is active
+		Session session = ParseFacebookUtils.getSession();
+		if (session != null && session.isOpened()) {
+
+		} else {
+			startLoginActivity();
+		}
 	}
 
 	@Override
@@ -31,5 +64,35 @@ public class MainActivity extends Activity {
 			break;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		if (currentUser != null) {
+
+		} else {
+			// If the user is not logged in, go to the
+			// activity showing the login view.
+			startLoginActivity();
+		}
+	}
+
+	private void onLogoutButtonClicked() {
+		// Log the user out
+		ParseUser.logOut();
+
+		// Go to the login view
+		startLoginActivity();
+	}
+
+	private void startLoginActivity() {
+		Intent intent = new Intent(this, LogInActivity.class);
+		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		startActivity(intent);
+		finish();
 	}
 }
---
+++ Mpp_Project/src/dk/aau/mpp_project/application/MyApplication.java
@@ -0,0 +1,44 @@
+package dk.aau.mpp_project.application;
+
+import com.parse.Parse;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseInstallation;
+import com.parse.PushService;
+
+import dk.aau.mpp_project.R;
+import dk.aau.mpp_project.activity.MainActivity;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+	// Parse parameters
+	public static final String	CHANNEL		= "colloc";
+	private static final String	ID_APP		= "S0ckBwXaRNgx9B87f5VE5GZGwUggjOVvj7NNLsAh";
+	private static final String	ID_CLIENT	= "hBicP6lpgj85DPwtgCnwBF8ZWs0xeNFwljqiJNqg";
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		
+		initParse();
+	}
+	
+	private void initParse() {
+		Parse.setLogLevel(Parse.LOG_LEVEL_VERBOSE);
+
+		// Needed to initialize Parse
+		Parse.initialize(this, ID_APP, ID_CLIENT);
+		// Needed for Facebook Login
+		ParseFacebookUtils.initialize(getString(R.string.app_id));
+
+		// Needed for push notifications
+		PushService.setDefaultPushCallback(this, MainActivity.class);
+		PushService.subscribe(this, CHANNEL, MainActivity.class);
+
+		// Installation of the phone (each phone as a unique installation)
+		ParseInstallation pi = ParseInstallation.getCurrentInstallation();
+		// If there is a connection, the installation is recorded in the Parse Database.
+		pi.saveEventually();
+	}
+}
