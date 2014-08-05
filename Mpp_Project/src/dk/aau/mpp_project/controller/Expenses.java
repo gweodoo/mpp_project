78bd5898f548ef223dcb8fa7e5e446f58298a6fb
tetouani63 <Jzn@h370.aau-conf-2-1b.wlan.aalborguniversitet.dk>
2014-08-05 11:24:24 +0200

    Update of expenses and operation class

--- Mpp_Project/src/dk/aau/mpp_project/controller/Expenses.java
+++ Mpp_Project/src/dk/aau/mpp_project/controller/Expenses.java
@@ -2,11 +2,26 @@ package dk.aau.mpp_project.controller;
 
 import java.util.List;
 
+import dk.aau.mpp_project.model.Operation;
+
 
 public class Expenses {
 	
 	public Expenses(){
 		
+	}
+	
+	public List<Operation> getAllExpenses()
+	{
+		List<Operation> operationsList = null;
+		
+		//connexion database
+		//get all operations from database
+		//close connexion
+		
+		Operation op = new Operation();
+		
+		return operationsList;
 	}
 	
 }
--- Mpp_Project/src/dk/aau/mpp_project/model/Operation.java
+++ Mpp_Project/src/dk/aau/mpp_project/model/Operation.java
@@ -8,7 +8,7 @@ public class Operation {
 	/* labels of atributs*/
 	public static final String OPERATION_ID = "operationId";
 	public static final String LENDER = "lender";
-	public static final String FLAT_ID = "flatId";
+	public static final String FLAT_ID = Flat.ID;
 	public static final String TO = "to";
 	public static final String COMMENT = "comment";
 	public static final String ISPAID = "isPaid";
@@ -19,4 +19,53 @@ public class Operation {
 		this.to = to;
 		this.flatId = flatId;
 	}
+
+	public int getOperationId() {
+		return operationId;
+	}
+
+	public void setOperationId(int operationId) {
+		this.operationId = operationId;
+	}
+
+	public int getLender() {
+		return lender;
+	}
+
+	public void setLender(int lender) {
+		this.lender = lender;
+	}
+
+	public int getFlatId() {
+		return flatId;
+	}
+
+	public void setFlatId(int flatId) {
+		this.flatId = flatId;
+	}
+
+	public int getTo() {
+		return to;
+	}
+
+	public void setTo(int to) {
+		this.to = to;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public Boolean getIsPaid() {
+		return isPaid;
+	}
+
+	public void setIsPaid(Boolean isPaid) {
+		this.isPaid = isPaid;
+	}
+	
 }
