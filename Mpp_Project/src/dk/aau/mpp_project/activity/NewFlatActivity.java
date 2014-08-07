a011b20408f1e974a4c94d1221b8d91be197249e
Fabrice Michaud <Fabrice@h163.aau-conf-2-2a.wlan.aalborguniversitet.dk>
2014-08-07 13:35:25 +0200

    New Flat Activity modified

--- Mpp_Project/src/dk/aau/mpp_project/activity/NewFlatActivity.java
+++ Mpp_Project/src/dk/aau/mpp_project/activity/NewFlatActivity.java
@@ -1,5 +1,12 @@
 package dk.aau.mpp_project.activity;
 
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardArrayAdapter;
+import it.gmariotti.cardslib.library.internal.CardHeader;
+import it.gmariotti.cardslib.library.view.CardListView;
+
+import java.util.ArrayList;
+
 import dk.aau.mpp_project.R;
 import dk.aau.mpp_project.R.layout;
 import dk.aau.mpp_project.R.menu;
@@ -13,6 +20,31 @@ public class NewFlatActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_flat);
+		
+		ArrayList<Card> cards = new ArrayList<Card>();
+
+		// Generating cards
+		for (int i = 0; i <= 20; i++) {
+			// Create a Card
+			Card card = new Card(getApplicationContext());
+			card.setTitle("This is a sample card...");
+			// Create a CardHeader
+			CardHeader header = new CardHeader(getApplicationContext());
+			header.setTitle("Card " + (i + 1));
+			// Add Header to card
+			card.addCardHeader(header);
+
+			cards.add(card);
+		}
+
+		CardArrayAdapter mCardArrayAdapter = new CardArrayAdapter(
+				getApplicationContext(), cards);
+
+		CardListView listView = (CardListView) findViewById(R.id.cardList);
+		
+		if (listView != null) {
+			listView.setAdapter(mCardArrayAdapter);
+		}
 	}
 
 	@Override
