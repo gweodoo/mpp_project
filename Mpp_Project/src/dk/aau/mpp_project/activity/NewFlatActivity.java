4cac5558d47191dcb1576506bd6220b0630ac613
Fabrice Michaud <Fabrice@h163.aau-conf-2-2a.wlan.aalborguniversitet.dk>
2014-08-08 16:28:40 +0200

    Menu button added

--- Mpp_Project/src/dk/aau/mpp_project/activity/NewFlatActivity.java
+++ Mpp_Project/src/dk/aau/mpp_project/activity/NewFlatActivity.java
@@ -16,6 +16,7 @@ import dk.aau.mpp_project.R.layout;
 import dk.aau.mpp_project.R.menu;
 import dk.aau.mpp_project.cards.ChooseFlatCard;
 import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
