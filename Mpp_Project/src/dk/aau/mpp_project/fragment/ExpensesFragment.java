6491699f6d11817b874370c6fc300cb1546ac2f5
tetouani63 <barrouho@ece.fr>
2014-08-11 15:47:51 +0200

    modif fragment_expenses

--- Mpp_Project/src/dk/aau/mpp_project/fragment/ExpensesFragment.java
+++ Mpp_Project/src/dk/aau/mpp_project/fragment/ExpensesFragment.java
@@ -7,14 +7,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 import de.greenrobot.event.EventBus;
 import dk.aau.mpp_project.R;
+import dk.aau.mpp_project.activity.MainActivity;
 import dk.aau.mpp_project.adapter.OperationAdapter;
 import dk.aau.mpp_project.database.DatabaseHelper;
 import dk.aau.mpp_project.event.FinishedEvent;
 import dk.aau.mpp_project.event.StartEvent;
 import dk.aau.mpp_project.model.Flat;
+import dk.aau.mpp_project.model.MyUser;
 import dk.aau.mpp_project.model.Operation;
 
 import java.util.ArrayList;
@@ -25,6 +31,8 @@ public class ExpensesFragment extends ListFragment implements FragmentEventHandl
 	private OperationAdapter mAdapter;
 	private Spinner idPeople;
     private ProgressDialog progressDialog;
+    private EditText commentText,amountText;
+    private String shareMate;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater,
@@ -32,7 +40,11 @@ public class ExpensesFragment extends ListFragment implements FragmentEventHandl
 		View expensesView = inflater.inflate(R.layout.fragment_expenses, container, false);
 		
 		idPeople = (Spinner) expensesView.findViewById(R.id.peopleSpinner);
+		commentText=(EditText)expensesView.findViewById(R.id.commentText);
+		amountText=(EditText)expensesView.findViewById(R.id.amountText);
 		
+		
+		//We fill the spinner with some infos useless...
         ArrayList<String> array = new ArrayList<String>();
         array.add("hi");
         array.add("ho");
@@ -45,19 +57,36 @@ public class ExpensesFragment extends ListFragment implements FragmentEventHandl
 		mAdapter=new OperationAdapter(getActivity(),R.layout.layout_expensesitem,myArray);
 		setListAdapter(mAdapter);
 		
-
+		//We fill the array with 20 entries useless
         Operation temp;
 		for(int i=0;i<20;i++)
 		{
 			temp=new Operation(new Flat(), "test", "test", 10.2,"hello", "hello", false);
 			
-
 			myArray.add(temp);
 			
 		}
         
 		mAdapter.notifyDataSetChanged();
 		// TODO Auto-generated method stub
+		
+		//We add a listener on the add button
+		ImageButton bt=(ImageButton)expensesView.findViewById(R.id.okButton);
+		bt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+            	
+            	MyUser user=((MainActivity)getActivity()).getMyUser();
+            	Flat flatId=((MainActivity)getActivity()).getMyFlat();
+            	
+            	//We create an operation object
+            	Operation temp;
+            	//We fill it
+            	temp=new Operation(flatId,user.getName(),shareMate,Double.valueOf(amountText.getText().toString()),"",commentText.getText().toString(),false);
+            
+            }
+		});
+		
 		return expensesView;
 	}
 
