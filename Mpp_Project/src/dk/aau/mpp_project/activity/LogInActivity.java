916ac48a4a59b4f4d25461b9006bc6b127124c2d
Fabrice Michaud <Fabrice@Fabrices-MacBook-Pro-Retina.local>
2014-08-13 15:54:38 +0200

    Big icon added

--- Mpp_Project/src/dk/aau/mpp_project/activity/LogInActivity.java
+++ Mpp_Project/src/dk/aau/mpp_project/activity/LogInActivity.java
@@ -34,7 +34,7 @@ public class LogInActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-
+		
 		initView();
 		initListener();
 
