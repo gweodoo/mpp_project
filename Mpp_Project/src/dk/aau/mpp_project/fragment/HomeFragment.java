9f7c00e3c49fe2135cf282e8c005217dfe9aa0db
Fabrice Michaud <Fabrice@h66.aau-conf-2-2a.wlan.aalborguniversitet.dk>
2014-08-13 10:00:09 +0200

    changed imegaview

--- Mpp_Project/src/dk/aau/mpp_project/fragment/HomeFragment.java
+++ Mpp_Project/src/dk/aau/mpp_project/fragment/HomeFragment.java
@@ -19,6 +19,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
+
+import com.koushikdutta.urlimageviewhelper.UrlImageViewHelper;
 import com.parse.ParseException;
 import de.greenrobot.event.EventBus;
 import dk.aau.mpp_project.R;
@@ -266,9 +268,8 @@ public class HomeFragment extends Fragment implements FragmentEventHandler,Swipe
 			viewHolder.comment.setText("\""+news.getComment()+"\"");
 			viewHolder.date.setText(news.getDate());
 //			viewHolder.photo.setImageDrawable(drawable)
-			System.out.println(news.getUser());
 //			MyUser user = (MyUser) news.get(News.USER);
-			UrlImageViewHelper.setUrlDrawable((CircularImageView)convertView.findViewById(R.id.photo), "http://graph.facebook.com/"+news.getUser().getFacebookId()+"/picture?type=large", R.drawable.image_user, 3600000);
+			UrlImageViewHelper.setUrlDrawable(viewHolder.photo, "http://graph.facebook.com/"+news.getUser().getFacebookId()+"/picture?type=large", R.drawable.image_user, 3600000);
 
 
 			return convertView;
